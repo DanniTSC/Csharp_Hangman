ef71ae7d7ad24bf3dc612dc03678f00ac973774d
Tascu_Daniel <tascudaniel22@stud.ase.ro>
2024-04-09 16:39:38 +0300

    Hello World in forms

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace TutorialYt
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Salut " + textBox1.Text + "!");
+        }
     }
 }
